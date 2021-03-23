10a8a58cedcdb9ba47d822261d4b8fe77a9fcea6
Ryan Rodrigues <71109880+RyanRodrigues880@users.noreply.github.com>
2021-03-23 09:03:05 -0300

    Add files via upload

---
+++ reserva.php
@@ -0,0 +1,16 @@
+﻿<?php
+session_start();
+$nome = $_POST['name'];
+$email = $_POST['email'];
+$telefone = $_POST['telefone'];
+$data = $_POST['data'];
+$quantidade = $_POST['quantidade'];
+$strcon = mysqli_connect('localhost','root','','lasorella');
+$sql = "INSERT INTO reservas (nome, email, telefone, data, quantidade) VALUES ('$nome', '$email', '$telefone', '$data', '$quantidade')"; 
+$resultado = mysqli_query($strcon,$sql);
+if ($resultado) {
+    echo "<script>alert('A reserva foi feita com sucesso, obrigado.');location.href=\"index.html\";</script>";
+} else {
+    echo "<script>alert('A reserva não foi feita, tente novamente.');location.href=\"reservar.html\";</script>";
+}
+?>
