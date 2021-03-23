061b3e8572ffd3fb897171c92f2a38d0bf31cb37
Ryan Rodrigues <71109880+RyanRodrigues880@users.noreply.github.com>
2021-03-23 09:04:01 -0300

    Update conexao.php

--- admin/conexao.php
+++ admin/conexao.php
@@ -1,8 +1,8 @@
 <?php
 $servidor = 'localhost';
-$banco = 'id16366396_lasorella';
-$usuario = 'id16366396_killer';
-$senha = '147258369Rr@';
+$banco = 'lasorella';
+$usuario = 'root';
+$senha = '';
 $conexao = mysqli_connect($servidor,$usuario,$senha);
 mysqli_select_db($conexao, $banco);
 ?>
